ca06395f3082ab172557da101db0f823b6b4ab92
Jonathan Gottfried <jonmarkgo@gmail.com>
2011-02-13 08:35:54 -0500

    Applied an open source design to it

--- index.php
+++ index.php
@@ -5,8 +5,13 @@
 
 
   <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.7/themes/ui-lightness/jquery-ui.css" type="text/css" media="all" />
+  <link rel="stylesheet" type="text/css" href="css/style.css" />
   </head>
-  <body style="background: url(mashme.jpg);">
+  <body>
+  <div id="container1">
+  <h1><span>MashMe</span></h1>
+
+<p>Let us mash you up.</p>
 <?php
 
 $app_id = '3144871';
@@ -14,7 +19,7 @@ $app_secret = 'da595545c095b5013243eaaa1b6fe4ee6e994556';
 
 
 if (!isset($_GET["auth_token_key"])) {
-print'<a class="button" href="http://hunch.com/authorize/v1/?app_id='.$app_id.'">Mash Me</a>';
+print'<center><a class="button" href="http://hunch.com/authorize/v1/?app_id='.$app_id.'" style="font-size: 26px;">Mash Me</a></center>';
 ?>
   <script>
   $(function() {
@@ -24,7 +29,7 @@ print'<a class="button" href="http://hunch.com/authorize/v1/?app_id='.$app_id.'"
   <?php
 } else {
   print'
-<div id="loading"><h1 id="masher">MASHING</h1><img src="ajax-loader.gif"></div>';
+<div id="loading"><center><h1 id="masher">MASHING STUFF UP</h1><img src="ajax-loader.gif" style="background:transparent;"></center></div>';
 ?>
 <script>
 $(function() {
@@ -44,5 +49,7 @@ $(function() {
 });
 </script>
 <?php } ?>
+<img src="echonest.gif" style="background: transparent; position: absolute; left:0; bottom: 0;"><img src="hunch.jpg" height="50" style="background: transparent; position: absolute; right:0; bottom: 0;">
+</div>
 </body>
 </html>
